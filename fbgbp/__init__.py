b85d62282120b06e79e049b4b0a35f6a6fd2cb43
Lioscro <phoenixter96@gmail.com>
2021-12-18 13:48:10 -0500

    Bump version: 0.0.0 → 0.0.1

--- fbgbp/__init__.py
+++ fbgbp/__init__.py
@@ -1,4 +1,4 @@
 # flake8: noqa
-__version__ = '0.0.0'
+__version__ = '0.0.1'
 
 from .grid_belief_propagation import FastBinaryGridBeliefPropagation
--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ to_cythonize = [
 requirements = read('requirements.txt').strip().split('\n')
 setup(
     name='fbgbp',
-    version='0.0.0',
+    version='0.0.1',
     url='https://github.com/Lioscro/fbgbp',
     author='Kyung Hoi (Joseph) Min',
     author_email='phoenixter96@gmail.com',
