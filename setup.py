db08afbdd04c3c38193413bcca97e64f9fc5e68c
Lioscro <phoenixter96@gmail.com>
2022-01-03 14:41:45 -0800

    Bump version: 0.0.5 → 0.0.6

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ to_cythonize = [
 requirements = read('requirements.txt').strip().split('\n')
 setup(
     name='fbgbp',
-    version='0.0.5',
+    version='0.0.6',
     url='https://github.com/Lioscro/fbgbp',
     author='Kyung Hoi (Joseph) Min',
     author_email='phoenixter96@gmail.com',
