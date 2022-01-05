2c3d0de53c9c61328d6f486d5eaea65f27df1f77
Lioscro <phoenixter96@gmail.com>
2022-01-05 09:31:37 -0800

    add macos specific compile args

--- setup.py
+++ setup.py
@@ -1,7 +1,8 @@
-from setuptools import find_packages, Extension, setup
+import platform
 
 import numpy as np
 from Cython.Build import cythonize
+from setuptools import find_packages, Extension, setup
 
 
 def read(path):
@@ -17,8 +18,9 @@ extra_compile_args = [
     '-Wno-unused-function',
     '-O3',
     '-std=c++11',
-    '-stdlib=libc++'
 ]
+if platform.system().lower() == 'darwin':
+    extra_compile_args.append('-stdlib=libc++')
 to_cythonize = [
     Extension(
         'fbgbp.grid_belief_propagation', [
